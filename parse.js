6f9f891a34c32cdd78c86f27332b8d09a5b2f251
Cherry <ilovecherries@protonmail.com>
2021-11-21 10:35:04 -0500

    mov video file support

--- parse.js
+++ parse.js
@@ -879,7 +879,7 @@ Parse.BLOCKS = {
 		function urlType(url) {
 			if (/(\.mp3(?!\w)|\.ogg(?!\w)|\.wav(?!\w)|#audio$)/.test(url))
 				return "audio"
-			if (/(\.mp4(?!\w)|\.mkv(?!\w)|#video$)/.test(url))
+			if (/(\.mp4(?!\w)|\.mkv(?!\w)|\.mov(?!\w)|#video$)/.test(url))
 				return "video"
 			if (/^(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?/.test(url))
 				return "youtube"
